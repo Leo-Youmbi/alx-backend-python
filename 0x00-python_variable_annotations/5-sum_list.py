1827254c1bbb95c0a72863bed9037bdab8982106
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-01-13 02:05:34 +0100

    feat: mandatory tasks

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Task 5's file.
+'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''Computes the sum of a list of float type numbers.
+    '''
+    return float(sum(input_list))
